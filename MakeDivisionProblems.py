cc28c5e22e6387e14672753868c067c6910f3130
GNUgeneration <eric@asciolla.com>
2016-11-06 11:35:16 -0500

    Add decimal capacity.

--- MakeDivisionProblems.py
+++ MakeDivisionProblems.py
@@ -1,10 +1,12 @@
 import random
 
-def MakeProblem(Divisor, Dividend):
+def MakeProblem(Divisor, Dividend, Decimal):
     print 'Divisor: %d' % Divisor
     print 'Dividend: %d' % Dividend
+    print 'Decimal: %d' % Decimal
 
 Divis = random.randint(100, 999)
 Divid = random.randint(100000, 999999999)
+Decim = random.randint(11, 99)
 
-print MakeProblem(Divis, Divid)
+print MakeProblem(Divis, Divid, Decim)
