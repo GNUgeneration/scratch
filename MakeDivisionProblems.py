c989233e48152e5ffd1eb069f8ecebccb371755a
GNUgeneration <eric@asciolla.com>
2016-11-07 13:07:17 -0500

    Add random decimal digits for each number.

--- MakeDivisionProblems.py
+++ MakeDivisionProblems.py
@@ -1,12 +1,17 @@
 import random
 
-def MakeProblem(Divisor, Dividend, Decimal):
-    print 'Divisor: %d' % Divisor
-    print 'Dividend: %d' % Dividend
-    print 'Decimal: %d' % Decimal
+def MakeProblem():
+    divisor = random.randint(100, 999)
+    dividend = random.randint(100000, 999999999)
+    divisor_decimals = random.randint(11, 99)
+    dividend_decimals = random.randint(11, 99)
+    print 'divisor: %d' % divisor
+    print 'dividend: %d' % dividend
+    print 
+    print 'divisor decimal digits: %d' % divisor_decimals
+    print 'dividend decimal digits: %d' % dividend_decimals
+    print 
+    return 'Have a nice day!'
+    
 
-Divis = random.randint(100, 999)
-Divid = random.randint(100000, 999999999)
-Decim = random.randint(11, 99)
-
-print MakeProblem(Divis, Divid, Decim)
+print MakeProblem()
